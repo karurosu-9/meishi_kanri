974e07af488c4665c8be8c3fd797082231aacc14
carlos <cl0xtc43x.apxma@gmail.com>
2023-01-21 14:46:44 +0900

    new file

---
+++ meishi_add.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Sample Programs</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <br>
+    <br>
+    名刺登録<br>
+    <br>
+    <br>
+    <br>
+    <form action="meishi_add_check.php" method="post">
+        <table>
+            <tr>
+                <td>お名前</td>
+                <td><input type="text" name="name" style="width:200px"></td>
+            </tr>
+            <tr>
+                <td>役職</td>
+                <td><input type="text" name="title" style="width:200px"></td>
+            </tr>
+            <tr>
+                <td>名前</td>
+                <td><input type="text" name="name" style="width:200px"></td>
+            </tr>
+            <tr>
+                <td>
+                    郵便番号<br>
+                    住所<br>
+                </td>
+                <td>
+                    <div class="h-adr">
+                        <span class="p-country-name" style="display:none;">Japan</span>
+                        <input type="text" class="p-postal-code" size="7" maxlength="7" style="width:200px"><br>
+                        <input type="text" class="p-region p-locality p-street-address p-extended-address" style="width:200px">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>電話番号</td>
+                <td><input type="text" name="tel" style="width:200px"></td>
+            </tr>
+            <tr>
+                <td>携帯番号</td>
+                <td><input type="text" name="mobile" style="width:200px"></td>
+            </tr>
+            <tr>
+                <td>email</td>
+                <td><input type="text" name="email" style="width:200px"></td>
+            </tr>
+        </table>
+        <input type="submit" value="確定">
+    </form>
+    <br>
+    <br>
+    <a href="meishi_list.php">名刺一覧へ戻る</a>
+    <br>
+<br>
+<br>
+</body>
+</html>
