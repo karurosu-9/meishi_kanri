801ad0dca960670e265c1233bb437d7494ce677e
carlos <cl0xtc43x.apxma@gmail.com>
2023-01-22 23:57:03 +0900

    update meishi_add_check

--- meishi_add_check.php
+++ meishi_add_check.php
@@ -8,5 +8,7 @@
     $tel=htmlspecialchars($_POST['tel'], ENT_QUOTES, "UTF-8");
     $mobile=htmlspecialchars($_POST['mobile'], ENT_QUOTES, "UTF-8");
     $email=htmlspecialchars($_POST['email'], ENT_QUOTES, "UTF-8");
+
+    $postal_mb= mb_convert_kana($postal, "a");
 ?>
 <?php require "footer.php";?>
